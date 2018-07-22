76e73762ae1ae9bdd99119734dddfaf184af0c8a
c-su <m.y.like.music.10@gmail.com>
2018-07-22 20:12:21 +0900

    view追加

--- controller/index.js
+++ controller/index.js
@@ -7,4 +7,19 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+router.get('/confirm', (req, res, next) => {
+  res.render('confirmEvent');
+});
+
+router.get('/event', (req, res, next) => {
+  res.locals.title = 'Hello World!';
+
+  res.render('createEvent');
+});
+
+router.post('/event', (req, res, next) => {
+
+  res.render('createdEvent');
+});
+
 module.exports = router;
